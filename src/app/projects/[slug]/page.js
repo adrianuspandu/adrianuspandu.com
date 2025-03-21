a724ef71bdbcd26a53ef7f78b08bbd27a75ff259
adrianuspandu <pwicaks@gmail.com>
2025-03-21 10:51:31 +0100

    added test mdx contents

--- src/app/projects/[slug]/page.js
+++ src/app/projects/[slug]/page.js
@@ -42,7 +42,7 @@ export default async function ProjectItemPage({ params }) {
 }
 
 export function generateStaticParams() {
-  return [{ slug: "test" }, { slug: "about" }];
+  return [{ slug: "jukeboxd" }, { slug: "my-mensa" }];
 }
 
 export const dynamicParams = true;
